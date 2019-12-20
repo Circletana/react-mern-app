c5dce72e267bb2373e6b4ab5878b2c99e1f4f7c8
Priyansh Balyan <priyanshbalyan@gmail.com>
2019-12-20 21:03:55 +0530

    Now uses dotenv as config

--- backend/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    MONGO_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/proj',
-
-    REDIS_URL: process.env.REDIS_URL || null,
-
-    TOKEN_SECRET: process.env.TOKEN_SECRET || 'secret',
-
-    API_PORT: process.env.PORT || 3001
-}
--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,50 @@
-"use strict";
+/* eslint-disable no-console */
+const path = require('path');
+require('dotenv').config({ path: '../.env' });
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const Config = require('./config');
 const cors = require('cors');
 
 const app = express();
+const fileUpload = require('express-fileupload');
 const router = require('./routes/api');
 const redisHelper = require('./helpers/redis');
-const path = require('path');
-const fileUpload = require('express-fileupload');
 
-const url = Config.MONGO_URI;
+const url = process.env.MONGO_URI;
 
-mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true }).then(mongo=>{
-    let database = mongo.connection.db;
-    database.collection('threads').createIndex({title: "text", description: "text"}).then().catch(err=>console.log(err));
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true }).then((mongo) => {
+	const database = mongo.connection.db;
+	if (database && database.collection) {
+		database.collection('threads').createIndex({ title: 'text', description: 'text' }).then().catch((err) => console.log(err));
+	}
 });
 
-let db = mongoose.connection;
+const db = mongoose.connection;
 
-db.once("open", () => console.log("connected to the database"));
-// db.on("error", err => console.log("MongoDB connection error:", err.message));
-db.on("error", err=> {throw new Error("MongoDB connection error "+err.message)});
+db.once('open', () => console.log('connected to the database'));
+db.on('error', console.log);
 
 redisHelper.init();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(fileUpload({ limits: { fileSize: 1 * 1024 * 1024 }, safeFileNames: true, preserveExtension: true }));
-app.use(logger("dev"));
+app.use(fileUpload({
+	limits: {
+		fileSize: 1 * 1024 * 1024,
+	},
+	safeFileNames: true,
+	preserveExtension: true,
+}));
 
-app.use(express.static(path.join(__dirname, "..", "client", "build" )));
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+app.use('/api', router);
+app.listen(process.env.API_PORT, () => console.log(`LISTENING ON PORT ${process.env.API_PORT}`));
 
-app.use("/api", router);
+process.on('uncaughtException', console.log);
+process.on('unhandledRejection', console.log);
 
-app.listen(Config.API_PORT, () => console.log(`LISTENING ON PORT ${Config.API_PORT}`));
+module.exports = app;
