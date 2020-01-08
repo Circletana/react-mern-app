c3f27ed1168ceecae8afc39c4c3171e3f5061ea4
Priyansh Balyan <priyanshbalyan@gmail.com>
2020-01-08 23:26:39 +0530

    Correct env variables

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,41 +1,40 @@
-const User = require('../models/user');
 const jwt = require('jwt-simple');
-const Config = require('../config');
 const bcrypt = require('bcrypt-nodejs');
+const User = require('../models/user');
 
-exports.signup = function(req, res) {
-    let user = new User({
-        email: req.body.email.toLowerCase()
-    });
-    user.password = user.generateHash(req.body.password);
-    user.save(err => {
-        if (err) {
-            console.log(err.message);
-            let msg = err.message;
-            if(err.code == 11000)
-                msg = "An account already exists with the email.";
-            return res.json({ status: "error", message: msg });
-        }
-        return res.json({ status: "success", user: user });
-    });
+// eslint-disable-next-line consistent-return
+exports.signup = async (req, res) => {
+	const data = req.body;
+	if (!data || !data.email || !data.password) {
+		return res.status(400).json({ status: 'error', message: 'Invalid parameters' });
+	}
 
-}
+	const user = new User({
+		email: req.body.email.toLowerCase(),
+	});
+	user.password = user.generateHash(req.body.password);
+	user.save((err) => {
+		if (err) {
+			// eslint-disable-next-line no-console
+			console.log(err.message);
+			let msg = err.message;
+			if (err.code === 11000) { msg = 'An account already exists with the email.'; }
+			return res.status(400).json({ status: 'error', message: msg });
+		}
+		return res.status(200).json({ status: 'success', user });
+	});
+};
 
-exports.login = function(req, res) {
-    User.findOne({ email: req.body.email }, (err, user) => {
-        if (err) {
-            console.log(err);
-            return res.json({ status: "error", message: err.message });
-        } else if (user && bcrypt.compareSync(req.body.password, user.password)) {
-            const token = jwt.encode({ email: user.email }, Config.TOKEN_SECRET);
-            res.json({ status: "success", data: { user: user, token: token } });
-        } else
-            res.json({ status: "error", message: "Invalid email/password" });
+exports.login = (req, res) => {
+	User.findOne({ email: req.body.email }, (err, user) => {
+		if (err) {
+			return res.json({ status: 'error', message: err.message });
+		} if (user && bcrypt.compareSync(req.body.password, user.password)) {
+			const token = jwt.encode({ email: user.email }, process.env.JWT_TOKEN_SECRET);
+			return res.status(200).json({ status: 'success', data: { user, token } });
+		}
+		return res.status(400).json({ status: 'error', message: 'Invalid email/password' });
+	});
+};
 
-    });
-}
-
-exports.user = function(req, res){
-    console.log(JSON.stringify(req.user));
-    return res.json({status:"success", data:req.user});
-}
+exports.user = (req, res) => res.json({ status: 'success', data: req.user });
