d9c62de936882d0d68281cab4cddf2d79a1f9c57
Priyansh Balyan <priyanshbalyan@gmail.com>
2020-01-11 17:49:41 +0530

    Eslint changes

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,36 +1,32 @@
-'use strict';
 
-let validator = require('validator');
-let bcrypt = require('bcrypt-nodejs');
-let mongoose = require('mongoose');
-let Schema = mongoose.Schema;
+const validator = require('validator');
+const bcrypt = require('bcrypt-nodejs');
+const mongoose = require('mongoose');
 
-let User = new Schema({
-    email: {
-        type: String,
-        unique: [true, "Email is already registered."],
-        required: [true, "Email is mandatory"],
-        trim: true,
-        lowercase: true,
-        validate: {
-            isAsync: false,
-            validator: validator.isEmail,
-            message: "Please fill a valid email address"
-        }
-    },
-    password: {
-        type: String,
-        trim: true,
-    }
+const { Schema } = mongoose;
+
+const User = new Schema({
+	email: {
+		type: String,
+		unique: [true, 'Email is already registered.'],
+		required: [true, 'Email is mandatory'],
+		trim: true,
+		lowercase: true,
+		validate: {
+			isAsync: false,
+			validator: validator.isEmail,
+			message: 'Please fill a valid email address',
+		},
+	},
+	password: {
+		type: String,
+		trim: true,
+	},
 
 });
 
-User.methods.generateHash = function(password) {
-    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
-};
+User.methods.generateHash = (password) => bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 
-User.methods.validPassword = function(password) {
-    return bcrypt.compareSync(password, this.password);
-};
+User.methods.validPassword = (password) => bcrypt.compareSync(password, this.password);
 
 module.exports = mongoose.model('User', User);
